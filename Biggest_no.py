b0b7e6e369bddbbbe508a9427e8d7f138cfaceb7
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-08-04 21:51:18 +0530

    Create Biggest_no.py

---
+++ Biggest_no.py
@@ -0,0 +1,9 @@
+# Input 
+lisit = [200, 7, 20, 71, 9, 3, 17, 8, 100, 57]
+big = (lisit[0])
+
+# Loop gets one chrarcter form the input
+for x in lisit:
+    if x > big :
+        big = x
+print (big)
