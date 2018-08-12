b8b5913b38d64ca09cfd4339a174d0643e89a452
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-08-12 22:51:27 +0530

    Create Odd_or_Even.py

---
+++ Odd_or_Even.py
@@ -0,0 +1,11 @@
+# input
+xi = input('enter a number: ')
+x = int(xi)
+
+# If input divied by 2 and remainder is 0 then input = to even
+if (x % 2 == 0) or x == 0:
+    print (xi +' is even')
+
+else:
+    print (xi +' is odd')
+print ('Done')
