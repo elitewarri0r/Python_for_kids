c15642185d854b641ee0198df12b623e11c8b473
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-08-12 22:42:28 +0530

    Create Names.py

---
+++ Names.py
@@ -0,0 +1,12 @@
+# input
+names = ['Rupa', 'Bharathi', 'Sriram', 'Srimathi']
+# printing the 3rd sting in the list
+print (names[2])
+
+# Printing names in the given order
+for a in names :
+    print (a)
+
+# Printing names in the given order with their position number
+for b in range(4):
+    print (b, names[b])
