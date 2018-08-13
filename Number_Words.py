27d7bfed18958578a024a1ecdcf66fd061444430
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-08-13 21:34:28 +0530

    Create Number_Words.py

---
+++ Number_Words.py
@@ -0,0 +1,9 @@
+# spelling for the numbers
+numbers = {0 : 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five', 6 : 'six',
+           7 : 'seven', 8 : 'eight', 9 : 'nine',}
+
+# input
+A = input("Enter a number: ")
+a = int(A)
+# giving the ouput ( spelling for the numbers from the map )
+print (numbers[a])
